f3b80aa5941d7b3e28bb31dae1f9a7a6b49b821d
Eduardo Salazar <eduardosalaz@outlook.com>
2020-08-02 17:31:38 -0500

    Usuario ahora es un Java Bean, cambiar metodos en el controlador para reflejar la nueva naturaleza

--- src/controladores/ControladorLogin.java
+++ src/controladores/ControladorLogin.java
@@ -15,77 +15,79 @@ import modelos.ModeloPantallaAdmin;
 
 public class ControladorLogin implements ActionListener {
 
-    VistaLogin vista;
-    ModeloLogin modelo;
-    public int intentos = 5;
-    
-    public ControladorLogin(VistaLogin vista, ModeloLogin modelo) {
-    	this.vista = vista;
-    	this.modelo = modelo;
-    	ejecutar();
-    }
-    public void ejecutar() {
-    	vista.lanzarVista();
-    	while(!vista.finished) {
-    		try {
-        		Thread.sleep(2);
-        	}catch(Exception e) {
-        		e.printStackTrace();
-        	}
-    	}
-    	agregarListeners();
-    	
-    	
-    }
+	VistaLogin vista;
+	ModeloLogin modelo;
+	public int intentos = 5;
+
+	public ControladorLogin(VistaLogin vista, ModeloLogin modelo) {
+		this.vista = vista;
+		this.modelo = modelo;
+		ejecutar();
+	}
+	public void ejecutar() {
+		vista.lanzarVista();
+		while(!vista.finished) {
+			try {
+				Thread.sleep(2);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		agregarListeners();
+
+
+	}
 
 
 
 	public void agregarListeners(){
-        vista.btn_login.addActionListener(this);
-    }
+		vista.btn_login.addActionListener(this);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == vista.btn_login) {
-        	boolean check = modelo.login(Integer.parseInt(vista.txt_usuario.getText()), new String(vista.pwd_password.getPassword()));
-        	if(intentos!=0) {
-        		if(check) {
-            		Usuario user = modelo.devolverUsuario();
-            		String nombre = user.Nombre;
-            		JOptionPane.showMessageDialog(null, "Bienvenido de nuevo " + nombre);
-            		if(user.Admin) {
-            			VistaPantallaUser vistaUser = new VistaPantallaUser();
-            			ModeloPantallaUser modeloUser = new ModeloPantallaUser();
-            			ControladorPantallaUser controladorUser = new ControladorPantallaUser(vistaUser, modeloUser);
-            			controladorUser.ejecutar();
-            		}else {
-            			VistaPantallaAdmin vistaAdmin = new VistaPantallaAdmin();
-            			ModeloPantallaAdmin modeloAdmin = new ModeloPantallaAdmin();
-            			ControladorPantallaAdmin controladorAdmin = new ControladorPantallaAdmin(vistaAdmin, modeloAdmin);
-            			controladorAdmin.ejecutar();
-            			
-            		}
-            	}else {
-            		JOptionPane.showMessageDialog(null, "Te quedan " + intentos + " para ingresar");
-            		intentos--;
-            		
-            	}
-        	}else {
-        		JOptionPane.showMessageDialog(null, "Has superado la cantidad de intentos permitidos, intenta después");
-        		vista.txt_usuario.setEnabled(false);
-        		vista.pwd_password.setEnabled(false);
-        		vista.btn_login.setEnabled(false);
-        		try {
-        			Thread.sleep(99999);
-        			vista.txt_usuario.setEnabled(true);
-        			vista.pwd_password.setEnabled(true);
-        			vista.btn_login.setEnabled(true);
-        			JOptionPane.showMessageDialog(null, "Puedes volver a intentarlo");
-        			intentos = 5;	
-        		}catch(Exception e1) {
-        			e1.printStackTrace();
-        		}
-        	}
-        }
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == vista.btn_login) {
+			int usuario = Integer.parseInt(vista.txt_usuario.getText());
+			String password = new String(vista.pwd_password.getPassword());
+			boolean check = modelo.login(usuario, password);
+			if(intentos!=0) {
+				if(check) {
+					Usuario user = modelo.devolverUsuario();
+					String nombre = user.getNombre();
+					JOptionPane.showMessageDialog(null, "Bienvenido de nuevo " + nombre);
+					if(user.isAdmin()) {
+						VistaPantallaUser vistaUser = new VistaPantallaUser();
+						ModeloPantallaUser modeloUser = new ModeloPantallaUser();
+						ControladorPantallaUser controladorUser = new ControladorPantallaUser(vistaUser, modeloUser);
+						controladorUser.ejecutar();
+					}else {
+						VistaPantallaAdmin vistaAdmin = new VistaPantallaAdmin();
+						ModeloPantallaAdmin modeloAdmin = new ModeloPantallaAdmin();
+						ControladorPantallaAdmin controladorAdmin = new ControladorPantallaAdmin(vistaAdmin, modeloAdmin);
+						controladorAdmin.ejecutar();
+
+					}
+				}else {
+					JOptionPane.showMessageDialog(null, "Te quedan " + intentos + " para ingresar");
+					intentos--;
+
+				}
+			}else {
+				JOptionPane.showMessageDialog(null, "Has superado la cantidad de intentos permitidos, intenta después");
+				vista.txt_usuario.setEnabled(false);
+				vista.pwd_password.setEnabled(false);
+				vista.btn_login.setEnabled(false);
+				try {
+					Thread.sleep(99999);
+					vista.txt_usuario.setEnabled(true);
+					vista.pwd_password.setEnabled(true);
+					vista.btn_login.setEnabled(true);
+					JOptionPane.showMessageDialog(null, "Puedes volver a intentarlo");
+					intentos = 5;
+				}catch(Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
 }
