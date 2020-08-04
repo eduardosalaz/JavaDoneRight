6ccbbaaf99d419e78bcd40d18634307577da2ca9
Eduardo Salazar <eduardosalaz@outlook.com>
2020-08-04 11:53:20 -0500

    Actualizar queries para los nuevos nombres en la DB

--- src/modelos/ModeloLogin.java
+++ src/modelos/ModeloLogin.java
@@ -20,30 +20,30 @@ public class ModeloLogin {
 		con = Conexion.Conectar();
 		//Query para saber si existe el usuario
 		try {
-			String query = "SELECT * FROM usuario WHERE ID_Usuario = ?";
+			String query = "SELECT * FROM usuario WHERE id_usr = ?";
 			pstm = con.prepareStatement(query);
 			pstm.setInt(1, user);
 			rs = pstm.executeQuery();
 			if(rs.next()) {
 				//Estoy super segurisimo que estoy haciendo algo mal y no tendria que hacer 2 queries pero por mientras funciona TODO: Arreglar las 2 queries
-				query = "SELECT * FROM usuario WHERE ID_Usuario =? AND Pass = AES_ENCRYPT(?, 'secret')";
+				query = "SELECT * FROM usuario WHERE id_usr =? AND pass = AES_ENCRYPT(?, 'secret')";
 				pstm = con.prepareStatement(query);
 				pstm.setInt(1,  user);
 				pstm.setString(2, pass);
 				rs = pstm.executeQuery();
 				if(rs.next()) {
 					//Query de la contraseña desencriptada
-					String query2 = "SELECT AES_DECRYPT(?, 'secret') as `contra` FROM usuario WHERE ID_USUARIO =?";
+					String query2 = "SELECT AES_DECRYPT(?, 'secret') as `contra` FROM usuario WHERE id_usr =?";
 					pstm2 = con.prepareStatement(query2);
 					pstm2.setString(1, pass);
 					pstm2.setInt(2, user);
 					rs2 = pstm2.executeQuery();
 					if(rs2.next()){
 						passW = rs2.getString("contra");
-						usuarios.add(new Usuario(rs.getInt("ID_Usuario"), rs.getBoolean("Admin"), passW, rs.getString("Nombre"), rs.getString("Telefono"), rs.getDate("Fecha_Alta"),
-								rs.getDate("Ultima_Sesion")));
+						usuarios.add(new Usuario(rs.getInt("id_usr"), rs.getBoolean("admin"), passW, rs.getString("nom_usr"), rs.getString("tel_usr"),
+								rs.getDate("alta_usr"), rs.getDate("ultima_usr")));
 						//Query de actualizar el campo de Ultima Sesion
-						query = "UPDATE usuario SET  Ultima_Sesion = NOW() WHERE ID_Usuario = ?";
+						query = "UPDATE usuario SET ultima_usr = NOW() WHERE id_usr = ?";
 						pstm = con.prepareStatement(query);
 						pstm.setInt(1, user);
 						pstm.executeUpdate();
