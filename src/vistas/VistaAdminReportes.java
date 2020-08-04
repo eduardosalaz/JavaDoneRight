3f44c1387b0fe4bfb00bedfe48b4c9fb2bb65531
Eduardo Salazar <eduardosalaz@outlook.com>
2020-08-04 12:20:46 -0500

    Archivos de vistas de admin, no están terminadas

---
+++ src/vistas/VistaAdminReportes.java
@@ -0,0 +1,43 @@
+package vistas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VistaAdminReportes {
+
+    private JFrame frame;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    VistaAdminReportes window = new VistaAdminReportes();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public VistaAdminReportes() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
